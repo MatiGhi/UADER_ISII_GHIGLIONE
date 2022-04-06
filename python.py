63c947b14e0dccc999d599625935d5be4bf0ca61
Matias Ghiglione <mghiglione@ombutech.net>
2022-04-06 11:13:17 -0300

    New feature added: data inputs can be pass as parameters via terminal

--- python.py
+++ python.py
@@ -1,34 +1,88 @@
-# prime number calculator: find all primes up to n
-max = int(input("Find primes up to what number? : "))
-primeList = []
-#for loop for checking each number
-for x in range(2, max + 1):
-	isPrime = True
-	index = 0
-	root = int(x ** 0.5) + 1
-	while index < len(primeList) and primeList[index] <= root:
-		if x % primeList[index] == 0:
-			isPrime = False
-			break
-		index += 1
-	if isPrime:
-		primeList.append(x)
-print(primeList)
-#-------------------------------------------------------------
-# prime number calculator: find the first n primes
-count = int(input("Find how many primes?: "))
-primeList = []
-x = 2
-while len(primeList) < count:
-	isPrime = True
-	index = 0
-	root = int(x ** 0.5) + 1
-	while index < len(primeList) and primeList[index] <= root:
-		if x % primeList[index] == 0:
-			isPrime = False
-			break
-		index += 1
-	if isPrime:
-		primeList.append(x)
-	x += 1
-print(primeList)
+import sys
+
+if len(sys.argv) == 1:
+        # prime number calculator: find all primes up to n
+		input("\nBienvenido/a! \nEste Script le permitirá encontrar los Números Primos deseados. Continuamos?")
+		max = int(input("Find primes up to what number? : "))
+		primeList = []
+		#for loop for checking each number
+		for x in range(2, max + 1):
+			isPrime = True
+			index = 0
+			root = int(x ** 0.5) + 1
+			while index < len(primeList) and primeList[index] <= root:
+				if x % primeList[index] == 0:
+					isPrime = False
+					break
+				index += 1
+			if isPrime:
+				primeList.append(x)
+		print(primeList)
+		#-------------------------------------------------------------
+		# prime number calculator: find the first n primes
+		count = int(input("Find how many primes?: "))
+		primeList = []
+		x = 2
+		while len(primeList) < count:
+			isPrime = True
+			index = 0
+			root = int(x ** 0.5) + 1
+			while index < len(primeList) and primeList[index] <= root:
+				if x % primeList[index] == 0:
+					isPrime = False
+					break
+				index += 1
+			if isPrime:
+				primeList.append(x)
+			x += 1
+		print(primeList)
+elif len(sys.argv) == 2:
+    argumentos = sys.argv
+    max = int(argumentos[1])
+    primeList = []
+    for x in range(2, max + 1):
+        isPrime = True
+        index = 0
+        root = int(x ** 0.5) + 1
+        while index < len(primeList) and primeList[index] <= root:
+            if x % primeList[index] == 0:
+                isPrime = False
+                break
+            index += 1
+        if isPrime:
+            primeList.append(x)
+    print(primeList)
+elif len(sys.argv) == 3:
+    argumentos = sys.argv
+    max = int(argumentos[1])
+    primeList = []
+    for x in range(2, max + 1):
+        isPrime = True
+        index = 0
+        root = int(x ** 0.5) + 1
+        while index < len(primeList) and primeList[index] <= root:
+            if x % primeList[index] == 0:
+                isPrime = False
+                break
+            index += 1
+        if isPrime:
+            primeList.append(x)
+    print(primeList)
+    #------------------------------------------#
+    # prime number calculator: find the first n primes
+    count = int(argumentos[2])
+    primeList = []
+    x = 2
+    while len(primeList) < count:
+        isPrime = True
+        index = 0
+        root = int(x ** 0.5) + 1
+        while index < len(primeList) and primeList[index] <= root:
+            if x % primeList[index] == 0:
+                isPrime = False
+                break
+            index += 1
+        if isPrime:
+            primeList.append(x)
+        x += 1
+    print(primeList)
